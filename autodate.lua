ec65b97c266f3befe884fbb9edd128fb9176bbfe
eliplaying <blade.elison@gmail.com>
2020-11-06 16:27:30 +0100

    file to set date automatically

---
+++ autodate.lua
@@ -0,0 +1,13 @@
+function Meta(m)
+    m.date = os.date("%Y%m%d", date)
+    return m
+  end
+
+
+-- function Meta(m)
+--     if(m.date == "\today")
+--         then
+--             m.date = os.date("%Y-%m-%d", date)
+--             return m
+--         end
+-- end
